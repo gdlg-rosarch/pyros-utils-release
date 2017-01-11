47f9258b52f04f81b765f3465e26acbabbda466c
alexv <asmodehn@gmail.com>
2017-01-11 13:29:48 +0900

    Rebase from 'debian/kinetic/pyros_utils'

--- pyros_utils/__init__.py
+++ pyros_utils/__init__.py
@@ -5,8 +5,10 @@ from ._version import __version__
 
 from .ros_utils import get_master, get_ros_home
 from .rostest_nose import rostest_or_nose_main
+from .get_ros_executed_file import get_ros_executed_file
 
 __all__ = [
     'get_master',
     'get_ros_home',
+    'get_ros_executed_file',
 ]
